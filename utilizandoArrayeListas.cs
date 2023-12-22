ff53019d1eb446fd0fe50ab592f73042d1cae1e7
Andressa Abrantes <andressasabrantes@gmail.com>
2023-12-22 06:57:02 -0300

    Convenções C# com desafios de código

---
+++ utilizandoArrayeListas.cs
@@ -0,0 +1,37 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Pergunta ao usuário quantos jogos deseja adicionar:
+        int quantidadeJogos = int.Parse(Console.ReadLine());
+
+        // TODO: Inicializa os arrays com base na quantidade informada pelo usuário:
+        string[] nomesJogos = new string[quantidadeJogos];
+       
+
+        // TODO: Crei um Loop para adicionar jogos conforme a quantidade especificada:
+       for (int i = 0; i < quantidadeJogos; i++)
+       {
+         AdicionarJogo(i, nomesJogos);
+       }
+
+        //TODO: Chame a função AdicionarJogo para obter o nome do jogo e armazená-lo no array:
+       
+
+        // Exibe o resumo da adição de jogos
+        ExibirResumoAdicaoJogos(quantidadeJogos, nomesJogos);
+    }
+
+    static void AdicionarJogo(int indice, string[] nomes)
+    {
+        // Entrada do nome do jogo
+        nomes[indice] = Console.ReadLine();
+    }
+
+    static void ExibirResumoAdicaoJogos(int quantidadeJogos, string[] nomes)
+    {
+        Console.WriteLine($"Foi adicionado '{quantidadeJogos}' jogos: {string.Join(", ", nomes)} ao catalogo.");
+    }
+}
